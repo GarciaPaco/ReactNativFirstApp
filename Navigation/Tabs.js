ff6eaea670eaaf3f1c200b65577f35a9afe97253
Enstryl <machinchivu@gmail.com>
2024-02-02 09:57:18 +0100

    Ajout de css sur les différentes navigations, ajouts du début de la logique pour gérer l'équipe à l'aide du AsyncStorage

--- Navigation/Tabs.js
+++ Navigation/Tabs.js
@@ -5,6 +5,11 @@ import Home from "../Screens/Home";
 import PokemonDetails from "../Components/PokemonDetails";
 import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
 import Settings from "../Pages/Settings";
+import MonEquipe from "../Pages/MonEquipe";
+import PokedexIcon from "../assets/pokedex.png";
+import {Image} from "react-native";
+import ParamsIcon from "../assets/settings.png";
+import MonEquipeIcon from "../assets/pokeball.png";
 
 
 const Stack = createNativeStackNavigator();
@@ -33,6 +38,22 @@ function MyTabs() {
                 headerTitleStyle: {
                     fontWeight: 'bold',
                 },
+                tabBarIcon: () => (
+                    <Image source={PokedexIcon} style={{width: 20, height: 20}}/>
+                ),
+            }}/>
+            <Tab.Screen name={"Mon équipe"} component={MonEquipe} options={{
+                title: 'Mon équipe',
+                headerStyle: {
+                    backgroundColor: '#ab0000',
+                },
+                headerTintColor: '#fff',
+                headerTitleStyle: {
+                    fontWeight: 'bold',
+                },
+                tabBarIcon: () => (
+                    <Image source={MonEquipeIcon} style={{width: 20, height: 20}}/>
+                ),
             }}/>
             <Tab.Screen name="Settings" component={Settings} options={{
                 title: 'Paramètres',
@@ -43,6 +64,9 @@ function MyTabs() {
                 headerTitleStyle: {
                     fontWeight: 'bold',
                 },
+                tabBarIcon: () => (
+                    <Image source={ParamsIcon} style={{width: 20, height: 20}}/>
+                ),
             }}/>
         </Tab.Navigator>
     );
